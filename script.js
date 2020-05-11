be0a47e19b770f503fcd09225ab13d7b870d9bfb
IVAN <ivanbatur5g@gmail.com>
2020-05-11 15:25:09 +0200

    Made an outline of the project

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let myLibrary = [];
+let addBook = document.querySelector("#addBook");
+addBook.addEventListener("click", (e) => {
+    displayForm(); //write this function
+});
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -11,4 +17,27 @@ function Book(title, author, pages, read) {
             return title + " by " + author + ", " + pages + " pages, not read yet";
         }
     }
+}
+
+function addBookToLibrary() {
+    let title = titleInput.value;
+    let author = authorInput.value;
+    let pages = pagesInput.value;
+    let read = readInput.value;
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+function render() {
+    myLibrary.forEach((book) => {
+        displayBook(book); //write this function
+    });
+}
+
+function displayBook(book) {
+
+}
+
+function displayForm() {
+    
 }
