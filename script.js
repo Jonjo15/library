a6bc892606e72303c3dd89f28bc1993b592d3b79
IVAN <ivanbatur5g@gmail.com>
2020-05-11 13:43:35 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        if (read) {
+            return title + " by " + author + ", " + pages + " pages, read";
+        }
+        else {
+            return title + " by " + author + ", " + pages + " pages, not read yet";
+        }
+    }
+}
